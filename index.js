57b0c3ea96ac0838a385ee69b6a9bf51cc063f46
smlsunxie <smlsun@gmail.com>
2019-06-20 16:21:23 +0800

    add ios sound params.

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ class SNSHelper {
                         alert: {
                             "body": message,
                             ...title ? { title } : {},
-                        }
+                        },
+                        sound: "default"
                     },
                     ...messageAttributes
                 }
